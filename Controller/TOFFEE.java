b8bf3d2aac30dce7c94a8cdea7ff72a7ef2458f5
Ahmed yehia <yehiaahmed900@gmail.com>
2023-05-11 14:33:02 +0200

    Changes by Mohannad

--- Controller/TOFFEE.java
+++ Controller/TOFFEE.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import ModelsClasses.Account;
